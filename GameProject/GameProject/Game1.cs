c702eab199fa2e4acd9ff0ec1eb24ab36f4d5e7e
csexton3 <banker.black@gmail.com>
2016-03-06 16:39:23 -0700

    Continuing Step 2.5

--- GameProject/GameProject/Game1.cs
+++ GameProject/GameProject/Game1.cs
@@ -221,9 +221,9 @@ namespace GameProject
             float randomVelocity = RandomNumberGenerator.NextFloat(GameConstants.BearSpeedRange) + GameConstants.MinBearSpeed;
 
             // Step 2.4 generate random angle
-            float randomAngle = RandomNumberGenerator.NextFloat((float)Math.PI * 180);
+            float randomAngle = RandomNumberGenerator.NextFloat((float)Math.PI / 180);
 
-            new Vector2 bearVector = Vector2();
+            Vector2 bearVector = new Vector2((float)Math.Sin(randomAngle));
 
             // create new bear
 
